b67826357a1db0b73eaf2e87df0361a37dc62e3b
IT21377358 <99610929+IT21377358@users.noreply.github.com>
2022-03-07 12:16:36 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -12,8 +12,22 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() {
-  
-  return 0;
-}
 
+int main()
+{
+    int n ;
+    int sum = 0;
+    
+    printf("Enter the n: ");
+    scanf("%d" , &n);
+    
+    printf("SUM = ");
+    for(int c=1; c<=n; c++)
+    {
+        sum = sum + c; 
+        printf("%d + " , c);
+    }
+    
+    printf("= %d" , sum);
+    return 0;
+}
